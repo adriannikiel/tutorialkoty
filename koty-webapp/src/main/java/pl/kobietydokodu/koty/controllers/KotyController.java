a7c8eb967a5666e91cacb88819914fe11ff229da
adriannikiel <adrian.nikiel@gmail.com>
2017-04-25 17:33:47 +0200

    added Spring MVC forms and validation

--- koty-webapp/src/main/java/pl/kobietydokodu/koty/controllers/KotyController.java
+++ koty-webapp/src/main/java/pl/kobietydokodu/koty/controllers/KotyController.java
@@ -1,12 +1,22 @@
 package pl.kobietydokodu.koty.controllers;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import pl.kobietydokodu.koty.KotDAO;
+import pl.kobietydokodu.koty.domain.Kot;
+import pl.kobietydokodu.koty.dto.KotDTO;
 
 @Controller
 public class KotyController {
@@ -20,10 +30,37 @@ public class KotyController {
 		return "lista";
 	}
 	
-	@RequestMapping("/dodaj")
-	public String dodajKota() {
+	@RequestMapping(value="/dodaj", method=RequestMethod.GET)
+	public String dodajKotaFormularz(@ModelAttribute("kotDto") @Valid KotDTO kotDto, BindingResult result) {
 		return "dodaj";
 	}
+	
+	@RequestMapping(value="/dodaj", method=RequestMethod.POST)
+	public String dodajKota(@ModelAttribute("kotDto") @Valid KotDTO kotDto, BindingResult result) {
+		if (!result.hasErrors()) {
+			//form filled correctly
+			Kot kot = new Kot();
+			SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+			
+			try {
+				kot.setDataUrodzenia(sdf.parse(kotDto.getDataUrodzenia()));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			
+			kot.setImie(kotDto.getImie());
+			kot.setImieOpiekuna(kotDto.getImieOpiekuna());
+			kot.setWaga(kotDto.getWaga());
+			kotDao.dodajKota(kot);
+			
+			return "redirect:/lista";
+		} 
+			
+		//form filled incorrectly
+		return "dodaj";			
+
+	}
+	
 	
 	@RequestMapping("/kot-{id}")
 	public String szczegolyKota(@PathVariable("id") Integer id, Model model) {
--- koty-webapp/src/main/webapp/WEB-INF/views/lista.jsp
+++ koty-webapp/src/main/webapp/WEB-INF/views/lista.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+ <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -16,14 +17,12 @@
 			</tr>
 		</thead>
 		<tbody>
-			<tr>
-				<th>0</th>
-				<th><a href="kot-0">Bonifacy</a></th>
-			</tr>
-			<tr>
-				<th>1</th>
-				<th><a href="kot-1">Tesla</a></th>
-			</tr>
+			<c:forEach items="${koty}" var="kot" varStatus="status">
+				<tr>
+					<td>${status.index}</td>
+					<td><a href="<c:url value="kot-${status.index}" />">${kot.imie}</a></td>
+				</tr>
+			</c:forEach>
 		</tbody>
 	</table>		
 	<br />
